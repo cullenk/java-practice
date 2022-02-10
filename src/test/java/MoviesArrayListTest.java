fd346e63a15b2f30778b9a98a176a13ec5850933
Cullen <cullenkuch@gmail.com>
2022-02-10 17:47:19 -0500

    added all tests

--- src/test/java/MoviesArrayListTest.java
+++ src/test/java/MoviesArrayListTest.java
@@ -3,18 +3,31 @@ import com.cullenkuch.MoviesArrayList;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertFalse;
 
 public class MoviesArrayListTest {
 
     @Test
-    void ContainsAddedMovie() {
+    void ContainsOneAddedMovie() {
         MoviesArrayList testMovieArray = new MoviesArrayList();
-        testMovieArray.addMovieToArray("Mean Girls");
-        testMovieArray.addMovieToArray("Bridesmaids");
-        assertTrue(testMovieArray.checkForMovie("Mean Girls"));
+        testMovieArray.addMovieToArray("Rebel Without A Cause");
+        assertTrue(testMovieArray.checkForMovie("Rebel Without A Cause"));
     }
 
     @Test
-    void
+    void ContainsTwoAddedMovies() {
+        MoviesArrayList testMovieArray = new MoviesArrayList();
+        testMovieArray.addMovieToArray("Mean Girls");
+        testMovieArray.addMovieToArray("Bridesmaids");
+        assertTrue(testMovieArray.checkForMovie("Bridesmaids"));
+    }
+
+    @Test
+    void DoesNotContainAddedMovie(){
+        MoviesArrayList testMovieArray = new MoviesArrayList();
+        testMovieArray.addMovieToArray("Halloween");
+        testMovieArray.addMovieToArray("Hereditary");
+        assertFalse(testMovieArray.checkForMovie("Scream"));
+    }
 
 }
